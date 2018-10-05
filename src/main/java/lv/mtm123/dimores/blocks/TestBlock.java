d73d361621ce730c351138ceae0244eb7045e9cd
MTM123 <matiss234@gmail.com>
2018-10-05 23:06:52 +0300

    Initial commit

---
+++ src/main/java/lv/mtm123/dimores/blocks/TestBlock.java
@@ -0,0 +1,21 @@
+package lv.mtm123.dimores.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+import static lv.mtm123.dimores.DimOres.MOD_ID;
+
+public class TestBlock extends Block {
+
+    public TestBlock(String name, Material mat) {
+        super(mat);
+        setHardness(2);
+        setHarvestLevel("pickaxe", 2);
+        setRegistryName(MOD_ID, name);
+        setCreativeTab(CreativeTabs.MATERIALS);
+        setTranslationKey("test");
+    }
+
+
+}
---
+++ src/main/java/lv/mtm123/dimores/items/TestItemBlock.java
@@ -0,0 +1,16 @@
+package lv.mtm123.dimores.items;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+
+import static lv.mtm123.dimores.DimOres.MOD_ID;
+
+public class TestItemBlock extends ItemBlock {
+
+    public TestItemBlock(Block block) {
+        super(block);
+        setRegistryName(MOD_ID, "test");
+        setTranslationKey("test");
+    }
+
+}
